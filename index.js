ed6e84b5925ec18d7dd1100f0894979e06f2228f
rovspogi48 <rovickdompor@gmail.com>
2022-11-26 22:12:49 +0800

    unang commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let HscoreEl = document.getElementById("Hscore-el")
+let GscoreEl = document.getElementById("Gscore-el")
+let Hscore = 0
+let Gscore = 0
+function Hplusone(){
+    Hscore += 1
+    HscoreEl.textContent = Hscore
+}
+function Gplusone(){
+    Gscore += 1
+    GscoreEl.textContent = Gscore
+}
+function Hplustwo(){
+    Hscore += 2
+    HscoreEl.textContent = Hscore
+}
+function Gplustwo(){
+    Gscore += 2
+    GscoreEl.textContent = Gscore
+}
+function Hplusthree(){
+    Hscore += 3
+    HscoreEl.textContent = Hscore
+}
+function Gplusthree(){
+    Gscore += 3
+    GscoreEl.textContent = Gscore
+}
+
